cba56bf1d54efab5de66333d092a333b22d3a2b2
Felix Zhang <38085633+Hogwarts250@users.noreply.github.com>
2020-03-16 11:53:59 -0700

    Solution to problem 18

--- 18.py
+++ 18.py
@@ -30,3 +30,35 @@ NOTE: As there are only 16384 routes, it is possible to solve this problem by tr
 challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 """
 
+def find_greater_sum(n1, n2, base):
+    if n1 > n2:
+        return n1 + base
+
+    else:
+        return n2 + base
+
+triangle = """
+75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+"""
+triangle = list(filter(None, [list(map(int, row.split())) for row in triangle.split("\n")]))
+triangle.reverse()
+
+for i, row in enumerate(triangle):
+    for j in range(len(row) - 1):
+        triangle[i+1][j] = find_greater_sum(row[j], row[j+1], triangle[i+1][j])
+
+print(triangle[-1][0])
