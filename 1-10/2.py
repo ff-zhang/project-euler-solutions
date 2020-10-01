b5fbb8e75a4f62bed792314394d948ef9875f3db
Sukhbir-4259 <69723681+Sukhbir-4259@users.noreply.github.com>
2020-10-01 11:27:20 +0530

    Update 2.py

--- 1-10/2.py
+++ 1-10/2.py
@@ -16,3 +16,21 @@ while b < 4 * 10 ** 6:
         total += b
 
 print(total)
+###by recurssion method###
+# Python program to display the Fibonacci sequence
+
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+nterms = 10
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
